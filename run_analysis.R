9b466248ed8e74136e2974537f81be4585241e76
Md. Morshedul Haque <morshedul.haque005@gmail.com>
2017-01-25 23:21:03 +0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 ## downloading and unzipping the dataset
-dir.create("./data")
+if(!dir.exists("./data")){dir.create("./data")}
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url = fileURL, destfile = "./data/Dataset.zip")
 
